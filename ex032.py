7f8d227c896c7e4db7742878a555e7ba37124bbb
Jonathan Eichenberger <161581454+jonathaneichenberger@users.noreply.github.com>
2024-07-23 13:23:44 -0300

    Create ex032.py

---
+++ ex032.py
@@ -0,0 +1,5 @@
+ano = int(input('Que ano gostaria de analisar se é bissexto? '))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} NÃO é BISSEXTO'.format(ano))
